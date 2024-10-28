1499cde6dcff017f38dcf51f959205188b801e6c
pablo gutierrez <pabloyucragutierrez@gmail.com>
2024-10-28 14:14:31 -0500

    movil

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  isMenuVisible: boolean = false;
+
   reloadPage() {
-    window.location.reload(); 
+    window.location.reload();
+  }
+
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
   }
 }
