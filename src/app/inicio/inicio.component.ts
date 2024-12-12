c8b1c821cc805c48b4f6eeeec6a67c4cfdd19b2c
pablo gutierrez <pabloyucragutierrez@gmail.com>
2024-12-12 12:35:37 -0500

    cambios

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -24,7 +24,7 @@ export class InicioComponent implements OnInit, AfterViewInit {
   }
 
   closeModal() {
-    this.showSuccessModal = false; 
+    this.showSuccessModal = false;
   }
 
   // contactForm: FormGroup;
@@ -57,23 +57,7 @@ export class InicioComponent implements OnInit, AfterViewInit {
   activeButton: 'left' | 'right' = 'right';
   hasAnimated: boolean = false;
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) // private fb: FormBuilder
-  {
-    // Inicialización del formulario reactivo
-    // this.contactForm = this.fb.group({
-    //   contactName: ['', Validators.required],
-    //   companyName: ['', Validators.required],
-    //   companyRuc: ['', Validators.required],
-    //   collaboratorsNum: ['', Validators.required],
-    //   country: ['Perú', Validators.required],
-    //   email: ['', [Validators.required, Validators.email]],
-    //   services: this.buildServices(),
-    //   comments: ['']
-    // });
-  }
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
     if ('IntersectionObserver' in window) {
@@ -104,15 +88,7 @@ export class InicioComponent implements OnInit, AfterViewInit {
     });
   }
 
-  // Construye un FormArray para los servicios
-  // buildServices(): FormArray {
-  //   const arr = this.services.map(() => this.fb.control(false));
-  //   return this.fb.array(arr);
-  // }
-
-  // get servicesArray(): FormArray {
-  //   return this.contactForm.get('services') as FormArray;
-  // }
+  
 
   animateNumbers() {
     const duration = 5000;
@@ -157,24 +133,4 @@ export class InicioComponent implements OnInit, AfterViewInit {
     this.activeButton = 'right';
   }
 
-  // Enviar el formulario
-  // onSubmit() {
-  //   const headers = new HttpHeaders().set('Content-Type', 'application/json');
-  //   this.http
-  //     .post<{ message: string }>(
-  //       'http://localhost:3000/new-contact/submit',
-  //       JSON.stringify(this.contactForm.value),
-  //       { headers }
-  //     )
-  //     .subscribe(
-  //       (response) => {
-  //         alert(response.message);
-  //         this.router.navigate(['/thank-you']);
-  //       },
-  //       (error) => {
-  //         console.error('Detalles del error:', error);
-  //         alert('Hubo un error al enviar el formulario. Intenta de nuevo más tarde.');
-  //       }
-  //     );
-  // }
 }
